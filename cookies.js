d17e37d5c4db12a9104e6e65da172513758a4436
Frank Xu <flx2@njit.edu>
2022-04-10 16:36:42 -0400

    new commit

--- cookies.js
+++ cookies.js
@@ -33,3 +33,12 @@ function checkCookie() {
     }
 }
 
+function deleteCookie(){
+    if( get_cookie( name ) ) {
+        document.cookie = name + "=" +
+            ((path) ? ";path="+path:"")+
+            ((domain)?";domain="+domain:"") +
+            ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+    }
+}
+
